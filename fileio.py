8b4902997dccdb2390ac36914e4d993dc561d3bf
Robert Kalescky <rkalescky@smu.edu>
2023-09-28 13:44:16 -0500

    Fix multiply sequence by float.

--- fileio.py
+++ fileio.py
@@ -1027,7 +1027,7 @@ class ProgressBar:
         #print s
         progress = self.width * self.count / self.total
         sys.stdout.write('{0:3}/{1:3}: '.format(self.count, self.total))
-        sys.stdout.write('>' * progress + '-' * (self.width - progress) + '\r')
+        sys.stdout.write('>' * int(progress) + '-' * int(self.width - progress) + '\r')
         if progress == self.width:
             sys.stdout.write('\n')
         sys.stdout.flush()
